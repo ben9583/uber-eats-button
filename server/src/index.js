7f812268389cb5ec95bf78b01787428eb1fd24c5
Ben Plate <bplate9583@gmail.com>
2024-02-03 12:16:48 -0700

    jwt validation

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: '../.env'});
@@ -9,10 +10,46 @@ if(process.env.SECRET_KEY === undefined) {
   process.exit(1);
 }
 
+const idempotentKeys = new Set();
+
+const jwtGuard = (jwt_token) => {
+  const token = jwt.verify(jwt_token, process.env.SECRET_KEY);
+  if(token.aud !== process.env.JWT_AUDIENCE || token.iss !== process.env.JWT_ISSUER || token.sub !== process.env.JWT_SUBJECT) {
+    throw new Error('Invalid token');
+  }
+  if(idempotentKeys.has(token.jti)) {
+    throw new Error('Idempotent key already used');
+  }
+  idempotentKeys.add(token.jti);
+}
+
 const app = express();
 
 app.get('/', (_, res) => {
   res.send('Hello World');
+})
+
+app.put('/order', (req, res) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    console.debug('No authorization header');
+    return res.status(401).send('Unauthorized');
+  }
+
+  const tokens = authorization.split(' ');
+  if(tokens.length !== 2 || tokens[0] !== 'Bearer') {
+    console.debug('Invalid authorization header');
+    return res.status(401).send('Unauthorized');
+  }
+
+  const jwt_token = tokens[1];
+  try { jwtGuard() } catch (error) {
+    console.debug('Invalid token');
+    return res.status(401).send('Unauthorized');
+  }
+
+  const token = jwt.decode(jwt_token);
+  
 })
 
 app.listen(3000, () => {
