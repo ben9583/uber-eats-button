48eb03e8e5e9d7bd66379d9fc005f5f698be6f65
Ben Plate <bplate9583@gmail.com>
2024-02-04 16:53:51 -0700

    implement risk distribution

--- server/src/utils.js
+++ server/src/utils.js
@@ -21,6 +21,26 @@ const weighted_random = (items, weights) => {
   return items[i];
 }
 
+/**
+ * Creates a discrete probability distribution for a given number of entries and a safety factor.
+ * If safety is 0, the distribution will be uniform. Otherwise, earlier entries will be more likely to be picked.
+ * Follows an exponential distribution; check function implementation for more details.
+ * @param {number} num_entries An positive integer number of entries
+ * @param {number} safety A nonnegative number, generally somewhere between 0 and 10
+ * @returns An array of length num_entries, where each entry is a number between 0 and 1 and the sum of all entries is 1.
+ */
+const risk_distribution = (num_entries, safety) => {
+  /*
+            (e^(s/n) - 1) * e^(s(n - x - 1)/n)
+  function: --------------------------------
+                        e^s - 1
+  */
+  if(safety === 0) return Array(num_entries).fill(1 / num_entries);
+  const sampler = x => (Math.exp(safety / num_entries) - 1) * Math.exp(safety * (num_entries - x - 1) / num_entries) / (Math.exp(safety) - 1);
+  return [...Array(num_entries).keys()].map(sampler);
+}
+
 module.exports = {
+  risk_distribution,
   weighted_random
 };
